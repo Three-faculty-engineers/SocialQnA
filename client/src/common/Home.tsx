809ab155773ddb1e296562ec159d795f35ca5ae9
uvidzadanje <davidmiskeljin@gmail.com>
2023-01-07 17:28:51 +0100

    Popravljen izgled

--- client/src/common/Home.tsx
+++ client/src/common/Home.tsx
@@ -6,6 +6,8 @@ import { CreatePost } from "../post/CreatePost";
 import { Post } from "../post/Post";
 import { PostDto } from "../post/post.dto";
 import { getByFollowers, getTop10 } from "../service/post.service";
+import { get } from "../service/user.service";
+import { UserDto } from "../user/user.dto";
 
 interface Props {
   auth: any;
@@ -14,6 +16,7 @@ interface Props {
 export default function Home(props: Props)
 {
   const [posts, setPosts] = useState([] as JSX.Element[]);
+  const [userAuthInfo, setUserAuthInfo] = useState({} as UserDto);
   const [top10Posts, setTop10Posts] = useState([] as JSX.Element[]);
 
   useEffect(() => {
@@ -22,6 +25,7 @@ export default function Home(props: Props)
 
   useEffect(() => {
     getPosts();
+    getAuthInfo();
   }, [props.auth]);
 
   async function getPosts()
@@ -38,6 +42,20 @@ export default function Home(props: Props)
     setPosts(result.data.map((post: PostDto, index: number) => <Post userID={props.auth.id} post={post} key={index}></Post>))
   }
 
+  async function getAuthInfo()
+  {
+    if(!props.auth.id) return;
+
+    const result = await get(props.auth.id);
+
+    if(!result.success)
+    {
+      return;
+    }
+
+    setUserAuthInfo(result.data);
+  }
+
   async function getTop10Posts()
   {
     const result = await getTop10();
@@ -50,32 +68,32 @@ export default function Home(props: Props)
     setTop10Posts(result.data.map((post: {id: string, title: string}, index: number) => <h3 key={index}><a href={`/post/${post.id}`}>{post.title}</a></h3>))
   }
 
-  if (!Object.keys(props.auth).length) {
-    return (
-      <Container>
-        <Row className="vh-100 d-flex justify-content-center my-5">
-          <Col md={8} lg={6} xs={12}>
-            <h1>Welcome!</h1>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
   return (
     <Container>
       <Row className="vh-100 my-5">
         <Col md={3} lg={3} xs={12}>
-
+          {!!Object.keys(props.auth).length && (
+            <h3><a href={`/profile/${userAuthInfo.id}`} className="text-decoration-none text-dark">{userAuthInfo.username}</a></h3>
+          )}
         </Col>
+
+        {!Object.keys(props.auth).length ? (
+          <Col md={6} lg={6} xs={12}>
+            <h1 className="text-center">Welcome!</h1>
+          </Col>
+        ) : (
+          
         <Col md={6} lg={6} xs={12}>
-          <CreatePost></CreatePost>
+          <div className="d-flex justify-content-center">
+            <CreatePost></CreatePost>
+          </div>
           {!posts.length && (
-            <h3>Trenutno nema nijedan post</h3>
+            <h3 className="text-center">Trenutno nema nijedan post</h3>
           ) || (
             posts
           )}
         </Col>
+        )}
         <Col md={3} lg={3} xs={12} className="text-center">
           <h2>Top 10 najcitanijih</h2>
           {top10Posts}
